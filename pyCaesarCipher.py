90e1db32b3a454ce6b987b2c7846130ae917910a
Achyut Singh <105210587+SinghAchyut@users.noreply.github.com>
2022-09-08 08:17:11 +0530

    Update pyCaesarCipher.py

--- pyCaesarCipher.py
+++ pyCaesarCipher.py
@@ -1,5 +1,6 @@
 from curses.ascii import isdigit
 
+# Caesar Cypher is a way to encrypt or decrypt messages used in the ancient Rome .
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print('''
